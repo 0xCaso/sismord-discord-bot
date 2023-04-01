d3d0eceb4d224a1d9a67fb68edfae1cf6ca0e9f0
Matteo Casonato <matteocasonato@protonmail.com>
2023-04-01 17:50:52 +0200

    Basic bot/server structure

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-// Load environment variables from .env file
 require('dotenv').config();
-
-// Import Discord.js library
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+const app = express();
 
-// Create a new client instance
+// Start the express server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
-// Listen for the "ready" event, which is triggered when the bot is connected
+// username => boolean (if verified or not)
+const membersMap = [];
+
 client.on(Events.ClientReady, () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Listen for the "guildMemberAdd" event, which is triggered when a new user joins the server
 client.once(Events.GuildMemberAdd, async (member) => {
-  // Create a private channel with the new user
-  const channel = await member.createDM();
-  
-  // Send a welcome message to the new user
-  channel.send('Welcome to the server! Click the following link to verify your membership: http://localhost:3000');
+    membersMap[member.user.username] = false;
+    const channel = await member.createDM();
+    channel.send('Welcome to the server! Click the following link to verify your membership: http://localhost:3000');
 });
 
-// Log in to the Discord API with the bot token
+app.post('/verify', (req, res) => {
+    const { username } = req.body;
+    membersMap[username] = true;
+    client.users.cache.find((user) => user.username === username).send('You have been verified!');
+    res.send('OK');
+});
+
 client.login(process.env.DISCORD_BOT_TOKEN);
